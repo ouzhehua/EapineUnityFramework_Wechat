fc7e11863210b076cdd44d1b7d1ac7cea1ab9efc
欧哲华 <ouzhehua@Awars-iMac-Pro.local>
2024-10-03 20:28:58 +0800

    font update

--- WeChatPlatform.cs
+++ WeChatPlatform.cs
@@ -24,23 +24,50 @@ namespace VisionzFramework.Runtime.WeChat
             Screen = new WeChatScreen();
             NetworkState = new WeChatNetworkState();
             InitFont();
-
-            if (m_InitCallback != null)
-            {
-                m_InitCallback(true);
-            }
-
-            m_InitCallback = null;
         }
 
-        protected void InitFont()
+        private void InitFont()
         {
-            WX.GetWXFont("https://shanmaiwangluo1.oss-cn-shenzhen.aliyuncs.com/download/SIMLI.TTF", LoadFontFinish);
+            try
+            {
+                WX.GetWXFont("https://shanmaiwangluo1.oss-cn-shenzhen.aliyuncs.com/download/SIMLI.TTF", LoadFontFinish);
+                //WX.GetWXFont("", LoadFontFinish);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("InitFont Error");
+                LoadFontFinish(null);
+                throw;
+            }
         }
 
+        private bool m_FontFinish = false;
         private void LoadFontFinish(Font font)
         {
-            TMPro.TMP_RuntimeFontUGUI.AddFontAsset(TMPro.TMP_RuntimeFontSettings.GetNickNameByIndex(0), font);
+            if (font != null)
+            {
+                TMPro.TMP_RuntimeFontUGUI.AddFontAsset(TMPro.TMP_RuntimeFontSettings.GetNickNameByIndex(0), font);
+            }
+            else
+            {
+                Debug.LogError("LoadFontFinish Error");
+            }
+            
+            m_FontFinish = true;
+            CheckFinish();
+        }
+
+        private void CheckFinish()
+        {
+            if (m_FontFinish)
+            {
+                if (m_InitCallback != null)
+                {
+                    m_InitCallback(true);
+                }
+
+                m_InitCallback = null;
+            }
         }
     }
 }
